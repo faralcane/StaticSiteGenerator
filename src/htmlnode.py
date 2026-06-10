a72970b20cd07079b4b15bbdf1752ba2ccb22ec8
faralcane <faralcane@gmail.com>
2026-06-09 22:50:50 -0400

    LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,8 +17,27 @@ class HTMLNode:
     def props_to_html(self):
         if self.props is None:
             return ""
-        props_str = " ".join(f'{key}="{value}"' for key, value in self.props.items())
-        return f" {props_str}" if props_str else ""
+        
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'        
+        return props_html
     
     def __repr__(self):        
-        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"    
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self, tag: str | None, value: str, props: dict[str, str] | None = None
+    ) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
