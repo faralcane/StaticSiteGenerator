f6cce33ca4e6d021104f101647988dcaa5b5a275
faralcane <faralcane@gmail.com>
2026-06-15 07:43:26 -0400

    Image and Link extraction

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes: list[TextNode] = []
@@ -19,3 +20,14 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 temp.append(TextNode(split_text[i], text_type))
         new_nodes.extend(temp)
     return new_nodes
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -4,6 +4,7 @@ from inline_markdown import (
 )
 
 from textnode import TextNode, TextType
+from inline_markdown import extract_markdown_images, extract_markdown_links
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -84,6 +85,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://wikipedia.org"),
+            ],
+            matches,
         )
 
 
